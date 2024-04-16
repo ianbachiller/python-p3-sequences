4159803b709f57d6fded684703cfcc6dc3ec7f6e
Ian Bachiller <ianbachise@gmail.com>
2024-04-16 14:40:30 -0400

    completed sequences lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+  if length == 0:
+    print([])
+  elif length == 1:
+    print([0])
+  else:
+    fibo = [0,1]
+    for _ in range(length-2):
+      fib = fibo[-1] + fibo[-2]
+      fibo.append(fib)
+    print(fibo)
